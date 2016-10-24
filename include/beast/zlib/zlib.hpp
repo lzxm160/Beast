de704acbd46fe7cfa05e32dded2edfbc41b0c851
Vinnie Falco <vinnie.falco@gmail.com>
2016-10-23 21:22:40 -0400

    [FOLD] z_Result

--- include/beast/zlib/zlib.hpp
+++ include/beast/zlib/zlib.hpp
@@ -121,22 +121,6 @@ enum class Flush
     trees
 };
 
-/* Return codes for the compression/decompression functions. Negative values
- * are errors, positive values are used for special but normal events.
- */
-enum z_Result
-{
-    Z_OK            =  0,
-    Z_STREAM_END    =  1,
-    Z_NEED_DICT     =  2,
-    Z_ERRNO         = -1,
-    Z_STREAM_ERROR  = -2,
-    Z_DATA_ERROR    = -3,
-    Z_MEM_ERROR     = -4,
-    Z_BUF_ERROR     = -5,
-    Z_VERSION_ERROR = -6
-};
-
 /* compression levels */
 enum z_Compression
 {
